07adf76f604db8b3cc70a373f568adfb8bc1b002
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-02-07 13:51:21 +0200

    complete create listing page UI

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState} from 'react'
+import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
 import { app } from '../firebase';
@@ -150,6 +151,9 @@ const Profile = () => {
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-98'>
           { loading ? 'Loading...' : 'Update'}
         </button>
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>
+          Create a new house
+        </Link>
       </form>
       <div className='flex justify-between mt-5 '>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
