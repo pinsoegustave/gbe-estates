b5bd044e7983ece9f8931fa7cf95d6f54a392e03
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-02-07 10:27:05 +0200

    Add sign out user functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState} from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
 import { app } from '../firebase';
-import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess } from '../redux/user/userSlice';
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess, signOutUserFailure, signOutUserSuccess, signOutUserStart } from '../redux/user/userSlice';
 
 const Profile = () => {
   const fileRef = useRef(null)
@@ -57,7 +57,6 @@ const Profile = () => {
     e.preventDefault();
     try {
       dispatch(updateUserStart());
-      
       const res = await fetch(`/api/user/update/${currentUser._id}`, {
         method: 'POST',
         headers: {
@@ -70,7 +69,6 @@ const Profile = () => {
         dispatch(updateUserFailure(data.message));
         return;
       }
-
       dispatch(updateUserSuccess(data));
       setUpdateSuccess(true)
     } catch (error) {
@@ -92,6 +90,21 @@ const Profile = () => {
     } catch (error) {
       dispatch(deleteUserFailure(error.message))
     }
+  }
+
+  const handleSignOut = async() => {    
+     try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+     } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+     }
   }
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -140,7 +153,7 @@ const Profile = () => {
       </form>
       <div className='flex justify-between mt-5 '>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
       <p className='text-green-700 mt-5'>{updateSuccess ? 'User is updated successfully' : ''}</p>
