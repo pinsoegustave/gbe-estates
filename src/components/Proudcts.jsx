342906fdd2f2d7f83eea57ddcd51196995f68314
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-02-15 11:29:26 +0200

    Complete delete user listing

--- src/components/Proudcts.jsx
+++ src/components/Proudcts.jsx
@@ -4,7 +4,8 @@ import Table from 'react-bootstrap/Table';
 
 export default function Proudcts() {
   const [ listing, setListing ] = useState(false);
-  const [ loading, setLoading ] = useState(true)
+  const [ loading, setLoading ] = useState(true);
+
   
   useEffect(() => {
     const getListing = async () => {
@@ -24,13 +25,30 @@ export default function Proudcts() {
     };
     getListing();
   }, []);
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      // setListing((prev) => prev.filter((listing) => result._id !== listingId));
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   
   return (
     <div> 
       <div className='container'>
         <h2>List of houses in the database</h2>
         <Link to='/layout/create-listing'>
-          <button className='btn btn-success my-3'>Create a new house +</button>
+          <button className='p-2 text-decoration-none bg-green-800 text-redBeige rounded-sm my-3'>Create a new house +</button>
         </Link>
         <Table striped="columns">
           <thead>
@@ -39,7 +57,7 @@ export default function Proudcts() {
               <th>Name</th>
               <th>Description</th>
               <th>Address</th>
-              <th>Regular Price</th>
+              <th>Furnished</th>  
               <th>Parking</th>
               <th>Type</th>
               <th>ImageUrls</th>
@@ -54,20 +72,20 @@ export default function Proudcts() {
                 <td>{result.name}</td>
                 <td>{result.description.slice(0,5) + ".."}</td>
                 <td>{result.address}</td>
-                {/* <td>{result.furnished === 'true' ? 'Furnished' : 'No'}</td> */}
+                <td>{result.furnished === 'true' ? 'Furnished' : 'No'}</td>
                 <td>{result.parking === 'true' ? 'True' : 'None'}</td>
                 <td>{result.type}</td>
                 {/* <td>{result.offer}</td> */}
                 <td><img src={result.imageUrls[0]} alt="" width={50} height={50} /></td>
-                <td className='gap-2'>
+                <td className='flex gap-2 mb-1'>
                 {/* <Link className='text-decoration-none btn btn-sm btn-success'  */}
                     {/* to={`/update/${info._id}`}> */}
                     {/* Update */}
                   {/* </Link> */}
-                    <button className='text-decoration-none bg-green-800 text-redBeige rounded-sm' onClick={e => handleDelete(info._id)}>
+                    <button className='p-2 text-decoration-none bg-green-800 text-redBeige rounded-sm'>
                       Update
                     </button>
-                  <button className='text-decoration-none bg-red-800 text-redBeige rounded-sm' onClick={e => handleDelete(info._id)}>
+                  <button className='p-2 text-decoration-none bg-red-800 text-redBeige rounded-sm' onClick={e => handleListingDelete(result._id)}>
                       Delete
                     </button>
                 </td>
